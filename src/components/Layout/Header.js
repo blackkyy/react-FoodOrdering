51b580ce458e0ce6864f2ddcea61f9fcd5fd3ff9
Amal <yakubov.amal@gmail.com>
2022-08-12 15:19:27 +0500

    #1 All components are created.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import styles from "./Header.module.css";
+import mealImg from "../../assets/meal-pic.jpg";
+import HeaderButton from "./HeaderButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>Healthy meals for You!</h1>
+        <HeaderButton/>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealImg} alt="Meals" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
